60efd5ce2dab39219b25770e22f278b7205a4b8b
siyamthandagwamanda <gwamandasiyamthanda13@gmail.com>
2026-08-31 09:32:27 +0200

    update about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -14,15 +14,23 @@ export default function About() {
             believes every great developer starts with curiosity and
             determination.
           </p>
+          
           <p>
             My journey into tech began as a creative escape. Drawing on my background in art, 
             I discovered a passion for transforming imaginative ideas into tangible digital experiences.
           </p>
+
           <p>
-            I've built a strong foundation in HTML, CSS, JavaScript, Git, and
-            GitHub. My next mission is to grow into a full stack developer —
-            continuously learning, solving problems, and creating technology
-            that helps people.
+            I have built a strong foundation in HTML, CSS, JavaScript, Git, and GitHub. To broaden this foundation,
+            I joined MLab as a Digital Solutions Trainee. 
+            Here, I expanded my skills to include SDLC principles, CRAP design principles, 
+            and Figma. I also developed vital communication skills through Scrum meetings, 
+            presentations, and expo work, I am currently in enrollment here until March 2027.
+          </p>
+
+          <p>
+           My next mission is to grow into a full-stack developer—continuously learning, 
+           solving problems, and creating technology that helps people.
           </p>
         </div>
       </div>
